3cdc18c8ff98d7750f5ae1df7efb617bdb8809f4
Jonas Santos <68215037+JonasHenriqueDev@users.noreply.github.com>
2023-07-23 00:03:02 -0300

    Initial commit

---
+++ src/main/java/com/jonas/redesocialbackend/RedeSocialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.redesocialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/redesocialbackend/RedeSocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.redesocialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
